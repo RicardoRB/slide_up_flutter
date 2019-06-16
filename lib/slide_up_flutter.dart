fa5629824f25bb7992f2e47173cf1d8a07d99b8a
RicardoRB <ricardoromebeni@gmail.com>
2019-06-16 17:26:00 +0200

    Initial commit

---
+++ lib/slide_up_flutter.dart
@@ -0,0 +1,98 @@
+library slide_up_flutter;
+
+import 'dart:math' as math;
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class BottomSlideUpSheet extends StatefulWidget {
+  final _BottomSlideUpState exhibitionBottomSheetState = _BottomSlideUpState();
+
+  @override
+  _BottomSlideUpState createState() => exhibitionBottomSheetState;
+}
+
+class _BottomSlideUpState extends State<BottomSlideUpSheet>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  double minHeight = 50;
+
+  double get maxHeight => MediaQuery.of(context).size.height;
+
+  double get headerTopMargin =>
+      lerp(20, 20 + MediaQuery.of(context).padding.top);
+
+  double get headerFontSize => lerp(14, 24);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  double lerp(double min, double max) =>
+      lerpDouble(min, max, _controller.value);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Positioned(
+          height: lerp(minHeight, maxHeight),
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: GestureDetector(
+            onTap: () => _toggle,
+            onVerticalDragUpdate: _handleDragUpdate,
+            onVerticalDragEnd: _handleDragEnd,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              decoration: const BoxDecoration(
+                color: Color(0xFF162A49),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+              ),
+              child: Column(
+                children: <Widget>[],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _toggle() {
+    final bool isOpen = _controller.status == AnimationStatus.completed;
+    _controller.fling(velocity: isOpen ? -2 : 2);
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    _controller.value -= details.primaryDelta / maxHeight;
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    if (_controller.isAnimating ||
+        _controller.status == AnimationStatus.completed) return;
+
+    final double flingVelocity =
+        details.velocity.pixelsPerSecond.dy / maxHeight;
+    if (flingVelocity < 0.0)
+      _controller.fling(velocity: math.max(2.0, -flingVelocity));
+    else if (flingVelocity > 0.0)
+      _controller.fling(velocity: math.min(-2.0, -flingVelocity));
+    else
+      _controller.fling(velocity: _controller.value < 0.5 ? -2.0 : 2.0);
+  }
+}
